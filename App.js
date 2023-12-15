d337aeda430658e86fe06c1d074368b3603eb243
Yongho5580 <70843139+Yongho5580@users.noreply.github.com>
2023-12-15 19:34:25 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import SearchInput from "./SearchInput.js"
+
+// App.js
+export default function App({ $target }) {
+    this.state = {
+      fetchedLanguages: [],
+      selectedLanguages: []  
+    }
+  
+    this.setState = (nextState) => {
+      // TODO: 구현해야함
+    }
+
+    const searchInput = new SearchInput({
+        $target,
+        initialState: ''
+      })
+  }
