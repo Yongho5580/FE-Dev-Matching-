ee86c6dc1f14f877788ea9d1ddc522cb21002efe
Yongho Kim <drata313@gmail.com>
2023-12-27 22:50:36 +0900

    [Updated]: Input, Suggestion 개발 및 API 연동

--- App.js
+++ App.js
@@ -1,18 +1,47 @@
-import SearchInput from "./SearchInput.js"
+import SearchInput from "./SearchInput.js";
+import Suggestion from "./Suggestion.js";
 
+import { fetchLanguages } from "./api.js";
 // App.js
 export default function App({ $target }) {
-    this.state = {
-      fetchedLanguages: [],
-      selectedLanguages: []  
-    }
-  
-    this.setState = (nextState) => {
-      // TODO: 구현해야함
-    }
+  this.state = {
+    fetchedLanguages: [],
+    selectedLanguages: [],
+  };
 
-    const searchInput = new SearchInput({
-        $target,
-        initialState: ''
-      })
-  }
+  this.setState = (nextState) => {
+    console.log(this.state, nextState);
+    this.state = {
+      ...this.state,
+      ...nextState,
+    };
+
+    suggestion.setState({
+      items: this.state.fetchedLanguages,
+    });
+  };
+
+  const searchInput = new SearchInput({
+    $target,
+    initialState: "",
+    onChange: async (keyword) => {
+      if (keyword.length === 0) {
+        this.setState({
+          fetchedLanguages: [],
+        });
+      } else {
+        const languages = await fetchLanguages(keyword);
+        this.setState({
+          fetchedLanguages: languages,
+        });
+      }
+    },
+  });
+
+  const suggestion = new Suggestion({
+    $target,
+    initialState: {
+      items: [],
+    },
+  });
+}
--- SearchInput.js
+++ SearchInput.js
@@ -1,19 +1,22 @@
-export default function SearchInput({
-    $target,
-    initialState
-  }) {
-    this.$element = document.createElement('form')
-    this.$element.className = "SearchInput"
-    this.state = initialState
-  
-    $target.appendChild(this.$element)
-  
-    this.render = () => {
-      this.$element.innerHTML = `
+export default function SearchInput({ $target, initialState, onChange }) {
+  // &target is document.querySelector('.App');
+  this.$element = document.createElement("form");
+  this.$element.className = "SearchInput";
+  this.state = initialState;
+
+  $target.appendChild(this.$element);
+
+  this.render = () => {
+    this.$element.innerHTML = `
       <input class="SearchInput__input" type="text" placeholder="프로그램 언어를 입력하세요." value="${this.state}">
-      `
-    }
-  
-    this.render()
-  
-  }
+      `;
+  };
+
+  this.render();
+
+  // 이벤트 핸들러 구현 부분
+
+  this.$element.addEventListener("keyup", (event) => {
+    onChange(event.target.value);
+  });
+}
